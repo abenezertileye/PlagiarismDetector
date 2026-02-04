63df7dfc649d8487c91113f9dbec8f04aead24ca
abene <abenezertileye2003@gmail.com>
2026-02-04 21:24:03 +0300

    new postgres used academic-assignment-helper/backend/database.py

--- academic-assignment-helper/backend/database.py
+++ academic-assignment-helper/backend/database.py
@@ -4,7 +4,7 @@ import os
 
 DATABASE_URL = os.getenv(
     "DATABASE_URL",
-    "postgresql://student:secure_password@localhost:5432/academic_helper"
+    "postgresql://postgres:123456@localhost:5433/academic_helper"
 )
 
 engine = create_engine(DATABASE_URL)
